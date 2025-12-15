a670c1eb3bb0ee89034a554a779deef03fc8a172
Aditya Dengale <adityadengale16@gmail.com>
2025-12-15 15:11:32 +0530

    Update sorting + searching.cpp

--- sorting + searching.cpp
+++ sorting + searching.cpp
@@ -1,9 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define MAX 100
+
 /* ---------- INSERTION SORT ---------- */
-void insertionSort(vector<int>& a) {
-    int n = a.size();
+void insertionSort(int a[], int n) {
     for (int i = 1; i < n; i++) {
         int j = i;
         while (j > 0 && a[j - 1] > a[j]) {
@@ -14,8 +15,7 @@ void insertionSort(vector<int>& a) {
 }
 
 /* ---------- BUBBLE SORT ---------- */
-void bubbleSort(vector<int>& a) {
-    int n = a.size();
+void bubbleSort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j + 1])
@@ -25,32 +25,32 @@ void bubbleSort(vector<int>& a) {
 }
 
 /* ---------- MERGE SORT ---------- */
-void merge(vector<int>& a, int low, int mid, int high) {
-    vector<int> temp;
-    int i = low, j = mid + 1;
+void merge(int a[], int low, int mid, int high) {
+    int temp[MAX];
+    int i = low, j = mid + 1, k = low;
 
     while (i <= mid && j <= high) {
-        if (a[i] <= a[j]) temp.push_back(a[i++]);
-        else temp.push_back(a[j++]);
+        if (a[i] <= a[j]) temp[k++] = a[i++];
+        else temp[k++] = a[j++];
     }
 
-    while (i <= mid) temp.push_back(a[i++]);
-    while (j <= high) temp.push_back(a[j++]);
+    while (i <= mid) temp[k++] = a[i++];
+    while (j <= high) temp[k++] = a[j++];
 
-    for (int k = low; k <= high; k++)
-        a[k] = temp[k - low];
+    for (int x = low; x <= high; x++)
+        a[x] = temp[x];
 }
 
-void mergeSort(vector<int>& a, int low, int high) {
+void mergeSort(int a[], int low, int high) {
     if (low >= high) return;
-    int mid = low + (high - low) / 2;
+    int mid = (low + high) / 2;
     mergeSort(a, low, mid);
     mergeSort(a, mid + 1, high);
     merge(a, low, mid, high);
 }
 
 /* ---------- QUICK SORT ---------- */
-int partition(vector<int>& a, int low, int high) {
+int partition(int a[], int low, int high) {
     int pivot = a[low];
     int i = low, j = high;
 
@@ -63,7 +63,7 @@ int partition(vector<int>& a, int low, int high) {
     return j;
 }
 
-void quickSort(vector<int>& a, int low, int high) {
+void quickSort(int a[], int low, int high) {
     if (low < high) {
         int p = partition(a, low, high);
         quickSort(a, low, p - 1);
@@ -72,10 +72,10 @@ void quickSort(vector<int>& a, int low, int high) {
 }
 
 /* ---------- BINARY SEARCH ---------- */
-int binarySearch(vector<int>& a, int key) {
-    int low = 0, high = a.size() - 1;
+int binarySearch(int a[], int n, int key) {
+    int low = 0, high = n - 1;
     while (low <= high) {
-        int mid = low + (high - low) / 2;
+        int mid = (low + high) / 2;
         if (a[mid] == key) return mid;
         else if (a[mid] < key) low = mid + 1;
         else high = mid - 1;
@@ -84,8 +84,7 @@ int binarySearch(vector<int>& a, int key) {
 }
 
 /* ---------- FIBONACCI SEARCH ---------- */
-int fibonacciSearch(vector<int>& a, int key) {
-    int n = a.size();
+int fibonacciSearch(int a[], int n, int key) {
     int fib2 = 0, fib1 = 1;
     int fib = fib1 + fib2;
 
@@ -121,8 +120,7 @@ int fibonacciSearch(vector<int>& a, int key) {
 
 /* ---------- MAIN MENU ---------- */
 int main() {
-    vector<int> a;
-    int choice;
+    int a[MAX], n = 0, choice;
 
     do {
         cout << "\n--- Sorting + Searching Menu ---\n";
@@ -135,22 +133,22 @@ int main() {
         cin >> choice;
 
         if (choice == 1) {
-            int n;
             cout << "Enter number of elements: ";
             cin >> n;
-            a.resize(n);
             cout << "Enter elements:\n";
-            for (int i = 0; i < n; i++) cin >> a[i];
+            for (int i = 0; i < n; i++)
+                cin >> a[i];
         }
 
         else if (choice == 2) {
             int ch;
             cout << "1. Insertion Sort\n2. Bubble Sort\nChoose: ";
             cin >> ch;
-            if (ch == 1) insertionSort(a);
-            else bubbleSort(a);
+            if (ch == 1) insertionSort(a, n);
+            else bubbleSort(a, n);
+
             cout << "Sorted Array: ";
-            for (int x : a) cout << x << " ";
+            for (int i = 0; i < n; i++) cout << a[i] << " ";
             cout << endl;
         }
 
@@ -158,10 +156,11 @@ int main() {
             int ch;
             cout << "1. Merge Sort\n2. Quick Sort\nChoose: ";
             cin >> ch;
-            if (ch == 1) mergeSort(a, 0, a.size() - 1);
-            else quickSort(a, 0, a.size() - 1);
+            if (ch == 1) mergeSort(a, 0, n - 1);
+            else quickSort(a, 0, n - 1);
+
             cout << "Sorted Array: ";
-            for (int x : a) cout << x << " ";
+            for (int i = 0; i < n; i++) cout << a[i] << " ";
             cout << endl;
         }
 
@@ -172,8 +171,8 @@ int main() {
             cout << "1. Binary Search\n2. Fibonacci Search\nChoose: ";
             cin >> ch;
 
-            int idx = (ch == 1) ? binarySearch(a, key)
-                                : fibonacciSearch(a, key);
+            int idx = (ch == 1) ? binarySearch(a, n, key)
+                                : fibonacciSearch(a, n, key);
 
             if (idx != -1) cout << "Found at index " << idx << endl;
             else cout << "Not Found\n";
