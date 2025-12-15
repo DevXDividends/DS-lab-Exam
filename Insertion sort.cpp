115aebfd8a86c086bf5c84c2559c4ded9e8f22f8
Aditya Dengale <adityadengale16@gmail.com>
2025-12-15 13:19:15 +0530

    Update Insertion sort.cpp

--- Insertion sort.cpp
+++ Insertion sort.cpp
@@ -1,17 +1,32 @@
 Insertion sort 
 TC:o(n^2) SC:O(1)
-class Solution {
-  public:
-    void insertionSort(vector<int>& a) {
-        // code here
-        int n=a.size();
-        for(int i=1;i<n;i++){
-            int j=i;
-            while(j>0 && a[j-1]>a[j]){
-                swap(a[j-1],a[j]);
-                j--;
-            }
-        }
-        
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void insertion_sort(int a[],int n)
+{
+
+  for (int i = 0; i < n; i++) // i = index of current element from unsorted part
+  {
+    int j = i;
+    // Move current element to correct position in sorted part
+    while (j > 0 && a[j - 1] > a[j])
+    {
+      swap(a[j - 1], a[j]);
+      j--;
     }
-};
+  }
+
+  cout << "Sorted array: ";
+  for (int i = 0; i < 6; i++) // fixed loop size to 6 so all elements print
+  {
+    cout << a[i] << " ";
+  }
+}
+
+int main()
+{
+  int a[6] = {10, 9, 8, 7, 6, 5};
+  insertion_sort(a,6);
+  return 0;
+}
